e865447912737b9103b93fdf22502b9b6cdaf41b
Chris9694 <christian9694@hotmail.com>
2023-09-17 23:16:52 -0600

    draft 2

---
+++ script.js
@@ -0,0 +1,13 @@
+function setup() {
+    createCanvas(windowWidth, windowHeight);
+    noStroke()
+  }
+  
+  function draw() {
+
+
+    fill(255,150,0,100)
+    circle(mouseX,mouseY,random(20,60))
+   
+  }
+  
